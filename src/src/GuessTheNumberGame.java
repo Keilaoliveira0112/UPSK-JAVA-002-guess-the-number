aecd633ee38e5de4b1400b7e5db84df4394d24f4
Keila <keilaoliveira.copy@gmail.comm>
2023-11-07 15:24:44 -0300

    refactor: main method refactoring

--- src/src/GuessTheNumberGame.java
+++ src/src/GuessTheNumberGame.java
@@ -6,25 +6,26 @@ public class GuessTheNumberGame {
 
     public static void main(String[] args) {
         targetNumber = random.nextInt(100) + 1;
-        Player currentPlayer = new HumanPlayer("Keila");
+        Player humanPlayer = new HumanPlayer("Keila");
         Player computerPlayer = new ComputerPlayer("Computador");
 
+        Player currentPlayer = humanPlayer;
         while (true) {
             checkGuess(currentPlayer);
             if (currentPlayer.hasGuessed()) {
                 displayGameResult(currentPlayer);
                 break;
             }
-            currentPlayer = (currentPlayer instanceof HumanPlayer) ? computerPlayer : new HumanPlayer("Keila");
+            currentPlayer = (currentPlayer instanceof HumanPlayer) ? computerPlayer : humanPlayer;
         }
     }
 
     public static void checkGuess(Player player) {
         int guess = player.makeGuess();
         if (guess < targetNumber) {
-            System.out.println(player.getName() + " fez a suposição " + guess + " (Muito Baixa)");
+            System.out.println(player.getName() + " fez a aposta " + guess + " (Muito Baixa)");
         } else if (guess > targetNumber) {
-            System.out.println(player.getName() + " fez a suposição " + guess + " (Muito Alta)");
+            System.out.println(player.getName() + " fez a aposta " + guess + " (Muito Alta)");
         } else {
             player.setGuessed(true);
         }
@@ -32,7 +33,7 @@ public class GuessTheNumberGame {
 
     public static void displayGameResult(Player winner) {
         System.out.println("Fim do jogo!");
-        System.out.println(winner.getName() + " adivinhou o número " + targetNumber + " corretamente!");
+        System.out.println(winner.getName() + " advinhou o número " + targetNumber + " corretamente!");
         System.out.println("Tentativas de " + winner.getName() + ": " + winner.getGuesses());
     }
 }
